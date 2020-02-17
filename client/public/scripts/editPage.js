0f96027105575b155cf9dab3d27f19e968639949
TomasVis <38735601+TomasVis@users.noreply.github.com>
2020-02-17 13:30:00 +0200

    separate json file

--- client/public/scripts/editPage.js
+++ client/public/scripts/editPage.js
@@ -1,78 +1,68 @@
-"use strict";
-const form = document.getElementById('form');
-//form.onsubmit = submit;
+'use strict';
 
-
-document.getElementById("klikas").addEventListener('click', submitas)
-
-function submitas(event) {
-  var value = "bla"
-  var url = "http://localhost:3000/update-post"+ window.location.search;
-  var form =  generateForm()
-
-
-
-  var xhr = new XMLHttpRequest();
-  xhr.open("POST", url, true);
-  
-
-  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-  
-  xhr.onreadystatechange = function() {
-      if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-          // Request finished. Do processing here.
-      }
-  }
-  xhr.send(JSON.stringify(form));
-}
-
-function generateForm() {
-  let form = {}
-  form.id = document.getElementById("id").value
-  return form
-}
-
-(function () {
-  let isEdit = window.location.search;
-
-function loadId() {
-    let xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-      if (this.readyState === 4 && this.status === 200) {
-        fillForm(JSON.parse(this.response));
-      }
-    };
-    xhttp.open("GET", "http://localhost:3000/get-post" + window.location.search);
-    xhttp.send();
-};
-
-
-
-function fillForm(postValues) {
-    for (let key in postValues) {
-      if (document.getElementById(key)) {
-        let element = document.getElementById(key);
-
-        if (element.id === "content") {
-          element.innerHTML = postValues[key];
-        } else {
-          element.setAttribute("value", postValues[key]);
-        }
-      }
-    }
-};
-
+document.getElementById('save').addEventListener('click', submit);
+document.getElementById('delete').addEventListener('click', submit);
+let isEdit = window.location.search;
 if (isEdit) {
   loadId();
 }
 
 if (!isEdit) {
-  document.getElementById("delete").className = "hidden";
+  document.getElementById('delete').className = 'hidden';
 }
 
-function save () {
-  if (isEdit) {
+function submit(event) {
+  let url = 'http://localhost:3000/update-post' + window.location.search;
+  let form = generateForm();
+  if (event.target.id === 'delete') {
+    form.delete = 'delete';
+  }
+  let xhr = new XMLHttpRequest();
+  xhr.open('POST', url, true);
 
+  xhr.setRequestHeader('Content-type', 'application/json');
+  xhr.onreadystatechange = function() {
+    if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+
+      window.location = 'http://localhost:80/index.html';
+    }
+  };
+
+  xhr.send(JSON.stringify(form));
+}
+
+function generateForm() {
+  let form = {};
+  form.id = document.getElementById('post-id').value;
+  form.author = document.getElementById('author').value;
+  form.date = document.getElementById('date').value;
+  form.title = document.getElementById('title').value;
+  form.content = document.getElementById('content').value;
+  return form;
+}
+
+function loadId() {
+
+  let xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+        fillForm(JSON.parse(xhr.response)); 
+    }
+  };
+  xhr.open('GET', 'http://localhost:3000/get-post' + window.location.search);
+  xhr.send();
+}
+
+function fillForm(postValues) {
+  for (let key in postValues) {
+    if (document.getElementById(key)) {
+      let element = document.getElementById(key);
+
+      if (element.id === 'content') {
+        element.innerHTML = postValues[key];
+      } else {
+        element.setAttribute('value', postValues[key]);
+      }
+    }
   }
 }
-})();
--- server/server.js
+++ server/server.js
@@ -1,30 +1,39 @@
-const express = require("express");
-const cors = require("cors");
-const path = require('path');
+const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
+const fs = require('fs');
+
+
+let posts = JSON.parse(fs.readFileSync('./data.json'));
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }));
+var jsonParser = bodyParser.json()
+
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
 app.use(cors());
 app.use(express.static('public'))
 
 
 
-app.get("/posts", function(req, res) {
+app.get('/posts',urlencodedParser, function(req, res) {
   res.json(posts);
 });
 
-app.get("/get-post", function(req, res) {
-  let post = posts.find(x => x.id === Number(req.query.id))
-  res.json(post);
+app.get('/get-post',urlencodedParser, function(req, res) {
+  if(req.query.id) {
+    let post = posts.find(x => x.id === Number(req.query.id))
+
+    res.json(post);
+  }
+  else res.json("no-data")
+
 });
 
-app.post("/update-post", function(req, res) {
-  console.log(req)
-  console.log(req.query.id)
-  console.log(req.body)
-  if(req.body.hasOwnProperty("delete")) {
+app.post('/update-post',jsonParser, function(req, res) {
+  if(req.body.hasOwnProperty('delete')) {
 
     let index = posts.findIndex(obj => Number(obj.id) === Number(req.body.id))
     posts.splice(index,1)
@@ -38,18 +47,13 @@ app.post("/update-post", function(req, res) {
   else {
     posts.push(createNewPost(req.body))
   }
-  res.json("complete")
-  //res.sendFile('index.html', { root: path.join(__dirname, './public') });
+  res.json('complete')
 });
 
-// app.post("/", function(req, res) {
-//   posts.push(createNewPost(req.body))
-//   res.sendFile('index.html', { root: path.join(__dirname, './public') });
 
-// });
 
 app.listen(3000, function() {
-  console.log("CORS-enabled web server listening on port 3000");
+  console.log('CORS-enabled web server listening on port 3000');
 });
 
 let createNewPost = (body) => {
@@ -57,81 +61,18 @@ let createNewPost = (body) => {
     id: nextId,
     date: body.date,
     author: body.author,
-    photo:"https://randomuser.me/api/portraits/lego/1.jpg",
-    userTag: "Reviewer",
+    photo:'https://randomuser.me/api/portraits/lego/1.jpg',
+    userTag: 'Reviewer',
     views:0,
     answers:0,
     votes:0,
-    tags:["android", "apple", "apps", "google", "technology"],
-    category: "Mobile",
+    tags:['android', 'apple', 'apps', 'google', 'technology'],
+    category: 'Mobile',
     title: body.title,
     content: body.content
   }
   nextId++
 return newPost
 }
-
-let posts = [
-  {
-    id: 0,
-    date: "2014-06-09",
-    author: "Marry",
-    photo:"https://randomuser.me/api/portraits/lego/0.jpg",
-    userTag: "Proffesor",
-    views:70,
-    answers:3,
-    votes:0,
-    tags:["apps", "business", "glass", "google", "technology"],
-    category: "Technology",
-    title: "Select coordinates which fall within central",
-    content:
-      "I have a database of coordinates adipiscing elit. Proin facilisis est ut ultricies mattis. In vitae velit nec ante viverra convallis. Donec vehicula luctus euismod. Interdum et malesuada fames ac ante ipsum primis in faucibus."
-  },
-  {
-    id: 1,
-    date: "2015-01-01",
-    author: "admin",
-    photo:"https://randomuser.me/api/portraits/lego/1.jpg",
-    userTag: "Reviewer",
-    views:52,
-    answers:10,
-    votes:2,
-    tags:["android", "apple", "apps", "google", "technology"],
-    category: "Mobile",
-    title: "Select coordinates which fall within central",
-    content:
-      "I have a database of coordinates adipiscing elit. Proin facilisis est ut ultricies mattis. In vitae velit nec ante viverra convallis. Donec vehicula luctus euismod. Interdum et malesuada fames ac ante ipsum primis in faucibus."
-  },
-  {
-    id: 2,
-    date: "2015-02-20",
-    author: "Smith",
-    photo:"https://randomuser.me/api/portraits/lego/2.jpg",
-    userTag: "Train",
-    views:70,
-    answers:3,
-    votes:0,
-    tags:["billionaire", "business"],
-    category: "Music",
-    title: "Why isn't music free",
-    content:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin facilisis est ut ultricies mattis. In vitae velit nec ante viverra convallis. Donec vehicula luctus euismod. Interdum et malesuada fames ac ante ipsum primis in faucibus."
-  },
-  {
-    id: 3,
-    date: "2015-10-15",
-    author: "Marry",
-    photo:"https://randomuser.me/api/portraits/lego/0.jpg",
-    userTag: "Train",
-    views:70,
-    answers:3,
-    votes:0,
-    tags:["apps", "facebook", "marketing", "google", "social"],
-    category: "Music",
-    title: "How to become milionaire in the next 5 years",
-    content:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin facilisis est ut ultricies mattis. In vitae velit nec ante viverra convallis. Donec vehicula luctus euismod. Interdum et malesuada fames ac ante ipsum primis in faucibus."
-  }
-];
 
 let nextId = posts.length
