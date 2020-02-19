cb80d1daf0c0dfcdd4f909cbd4cd3e5d36184fbb
TomasVis <38735601+TomasVis@users.noreply.github.com>
2020-02-19 10:26:35 +0200

    semicolons

--- server/server.js
+++ server/server.js
@@ -3,13 +3,13 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const app = express();
-const jsonParser = bodyParser.json()
-const urlencodedParser = bodyParser.urlencoded({ extended: false })
+const jsonParser = bodyParser.json();
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 let posts = JSON.parse(fs.readFileSync('./data.json'));
 
 app.use(cors());
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 app.get('/posts',urlencodedParser, function(req, res) {
   res.json(posts);
@@ -17,31 +17,31 @@ app.get('/posts',urlencodedParser, function(req, res) {
 
 app.get('/get-post',urlencodedParser, function(req, res) {
   if(req.query.id) {
-    let post = posts.find(x => x.id === Number(req.query.id))
+    let post = posts.find(x => x.id === Number(req.query.id));
     res.json(post);
-  } else res.json("no-data")
+  } else res.json("no-data");
 
 });
 
 app.get('/delete-post',urlencodedParser, function(req, res) {
   if(req.query.id) {
-    let index = posts.findIndex(obj => Number(obj.id) === Number(req.query.id))
-    posts.splice(index,1)
-    res.json("post was deleted")
-  } else res.json("no post to delete")
+    let index = posts.findIndex(obj => Number(obj.id) === Number(req.query.id));
+    posts.splice(index,1);
+    res.json("post was deleted");
+  } else res.json("no post to delete");
 
 });
 
 app.post('/update-post',jsonParser, function(req, res) {
   if(req.query.id){
-      req.body.id = Number(req.query.id)
-      let index = posts.findIndex(obj => Number(obj.id) === Number(req.body.id))
+      req.body.id = Number(req.query.id);
+      let index = posts.findIndex(obj => Number(obj.id) === Number(req.body.id));
       let newObject = {...posts[index], ...req.body};
       posts[index] = newObject;
   } else {
-    posts.push(createNewPost(req.body))
+    posts.push(createNewPost(req.body));
   }
-  res.json('complete')
+  res.json('complete');
 });
 
 app.listen(3000, function() {
@@ -64,7 +64,7 @@ let createNewPost = (body) => {
     content: body.content
   }
   nextId++
-return newPost
+return newPost;
 }
 
-let nextId = posts.length
+let nextId = posts.length;
